68ae38f04279bc3b4395f094cbf1f6e76552bb83
Celeste Kilgore <celestekilgore@gmail.com>
2023-10-30 15:26:11 -0400

    post code review

--- part-3/App.js
+++ part-3/App.js
@@ -1,9 +1,9 @@
 function App() {
   return (
     <div>
-      <Person age="17" name="i have a really long name" hobbies={["soccer","cooking"]}/>
-      <Person age="55" name="Test2" hobbies={["art","swimming","hiking"]}/>
-      <Person age="19" name="Test3" hobbies={["voting","walking"]}/>
+      <Person age={17} name="i have a really long name" hobbies={["soccer","cooking"]}/>
+      <Person age={55} name="Test2" hobbies={["art","swimming","hiking"]}/>
+      <Person age={19} name="Test3" hobbies={["voting","walking"]}/>
     </div>
   );
 }
